8273496404befe5564916449f8f3aa9f3bd3f149
RafaelNevesdeOliveira <rafaelneves652@gmail.com>
2024-08-20 19:23:40 -0300

    notificação

--- App.tsx
+++ App.tsx
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Button } from "react-native";
+import { styles } from "./styles";
+import { useEffect, useState } from "react";
+import notifee, {
+  EventType,
+  TimestampTrigger,
+  TriggerType,
+} from "@notifee/react-native";
 
 export default function App() {
+  const [notificationId, setNotificationId] = useState<string | null>(null);
+
+  async function displayNotification() {
+    await notifee.requestPermission({
+      criticalAlert: true,
+    });
+
+    const id = await notifee.displayNotification({
+      id: "1",
+      title: "Olá Rafael",
+      body: "Essa é minha primeira notificação",
+      ios: {
+        sound: "local.wav",
+        critical: true,
+      },
+    });
+
+    setNotificationId(id);
+  }
+
+  async function updateNotification() {
+    if (notificationId) {
+      await notifee.displayNotification({
+        id: notificationId,
+        title: "Atualização Rafael",
+        body: "Esta é uma atualização da notificação",
+        ios: {
+          sound: "local.wav",
+          critical: true,
+        },
+      });
+      console.log("notificação atualizaca")
+    }else{
+      console.log('Nenhuma notificação para atualizar')
+    }
+  }
+
+  async function cancelNotification() {
+    if(notificationId){
+      await notifee.cancelNotification(notificationId);
+      console.log('notificação cancelada');
+    }else{
+      console.log('Nenhuma notificação para cancelar');
+    }
+  }
+
+
+  async function scheduleNotification() {
+    const date = new Date(Date.now());
+    date.setSeconds(date.getSeconds() + 10);
+
+    const trigger: TimestampTrigger = {
+      type: TriggerType.TIMESTAMP,
+      timestamp: date.getTime(),
+    };
+
+    await notifee.createTriggerNotification(
+      {
+        id: '2',
+        title: "Notificação Agendada",
+        body: 'Aplicativo em segundo plano!',
+        ios: {
+          sound: "local.wav",
+          critical: true,
+        },
+      },
+      trigger
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Notificações</Text>
+      <Button title="Enviar Notificação" onPress={displayNotification}></Button>
+      <Button title="Atualizar Notificação" onPress={updateNotification}></Button>
+      <Button title="Cancelar Notificação" onPress={cancelNotification}></Button>
+      <Button title="Agendar Notificação" onPress={scheduleNotification}></Button>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Readme.md
@@ -0,0 +1,9 @@
+## Definição
+- Notificações em mobile são alertas enviados para dispositivos móveis, informando os usuários sobre atualizações, mensagens ou eventos importantes, mesmo quando o aplicativo está fechado. Elas podem aparecer na barra de notificações (Push Notifications) ou dentro do app enquanto ele está sendo usado (In-app Notifications).
+
+- **`npx create-expo-app localNotification --template`**
+- **`npx expo start`**
+- **`npm install --save @notifee/react-native`**
+- **`npx expo install expo-build-properties`**
+- **`npx expo prebuild --clean`**
+- **`npx expo run:ios`**
